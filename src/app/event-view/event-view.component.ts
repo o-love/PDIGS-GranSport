2088b37c03a2596b9afc732857ad6a5b984e951c
Oscar Love <olabradalove@gmail.com>
2024-04-02 11:38:09 +0100

    Some basic structure

---
+++ src/app/event-view/event-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-view.component.html',
+  styleUrl: './event-view.component.css'
+})
+export class EventViewComponent {
+
+}
